1c54857c02ff29a697f2cc3f53cc0db9d20b33cf
이상운 <sangunlee6@gmail.com>
2023-06-19 03:02:34 +0900

    feat: 새로운 DB 구조에 맞게 function/deleteKeyWord 수정

--- function/deleteKeyWord.js
+++ function/deleteKeyWord.js
@@ -2,36 +2,31 @@ require("dotenv").config();
 const { MongoClient } = require("mongodb");
 const conn_str = process.env.mongoURI;
 
-
-
-
-const deleteKeyWord = async (username, keyWord, date, res) => {
+const deleteKeyWord = async (username, keyWord, date) => {
   try {
     const client = await MongoClient.connect(conn_str);
     const database = client.db("search");
     const userScrapCollection = database.collection(username);
-    const result = await userScrapCollection.findOne({ user: username });
 
-    // keyWord에 해당하는 데이터 삭제
-    result.keyWords = result.keyWords.filter(
-      (keyword) => !(keyword.date === date && keyword.keyWord === keyWord)
-    );
+    // 데이터 삭제
+    const deletionResult = await userScrapCollection.deleteMany({
+      keyWord: keyWord,
+    });
 
-    if (result.keyWords.length === 0) {
-      // keyWords 배열이 모두 비었을 경우 컬렉션 삭제
+    if (deletionResult.deletedCount === 0) {
+      return { message: "삭제된 데이터가 없습니다." };
+    }
+
+    const count = await userScrapCollection.countDocuments();
+    if (count === 0) {
       await userScrapCollection.drop();
     }
 
-    await userScrapCollection.updateOne(
-      { user: username },
-      { $set: { keyWords: result.keyWords } }
-    );
     return { message: "데이터 삭제 완료" };
   } catch (error) {
     console.error(error);
-    res.status(500).json("스크랩 삭제 오류");
+    return { message: "데이터 삭제 에러" };
   }
 };
 
 module.exports = { deleteKeyWord };
-

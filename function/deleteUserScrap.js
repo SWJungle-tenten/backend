4b919a0b6f0fe1da4d9d3e9d3bcfcec6086c329e
이상운 <sangunlee6@gmail.com>
2023-06-19 03:05:59 +0900

    feat: 새로운 DB 구조에 맞게 function/deleteUserScrap 수정

--- function/deleteUserScrap.js
+++ function/deleteUserScrap.js
@@ -1,48 +1,35 @@
-require('dotenv').config();
-const { MongoClient } = require('mongodb');
+require("dotenv").config();
+const { MongoClient } = require("mongodb");
 const conn_str = process.env.mongoURI;
 
 // 스크랩 하나 삭제
 const deleteUserScrap = async (username, url, title, date) => {
   try {
     const client = await MongoClient.connect(conn_str);
-    const database = client.db('search');
+    const database = client.db("search");
     const userScrapCollection = database.collection(username);
-    const result = await userScrapCollection.findOne({ user: username });
-    let dataDeleted = false;
 
-    // keyWords 배열을 순회하면서 데이터 삭제
-    for (let i = 0; i < result.keyWords.length; i++) {
-      const keywordData = result.keyWords[i].data;
-      const keywordDate = result.keyWords[i].date;
-      if (keywordDate !== date) {
-        continue;
-      } else {
-        const dataIndex = keywordData.findIndex((item) => item.title === title && item.url === url);
-        if (dataIndex !== -1) {
-          keywordData.splice(dataIndex, 1);
-          dataDeleted = true;
+    // 데이터 삭제
+    const deletionResult = await userScrapCollection.deleteOne({
+      user: username,
+      url: url,
+      title: title,
+      date: date,
+    });
 
-          if (keywordData.length === 0) {
-            // 데이터 배열이 비었을 경우 해당 키워드 삭제
-            result.keyWords.splice(i, 1);
-            if (result.keyWords.length === 0) {
-              // keyWords 배열이 모두 비었을 경우 컬렉션 삭제
-              await userScrapCollection.drop();
-            }
-          }
-          break; // 삭제 완료 후 루프 종료
-        }
-      }
+    if (deletionResult.deletedCount === 0) {
+      return { message: "삭제된 데이터가 없습니다." };
     }
-    if (!dataDeleted) {
-      // 해당 데이터가 없을 경우 에러 응답
-      return { message: '해당 데이터를 찾을 수 없음' };
+
+    const count = await userScrapCollection.countDocuments();
+    if (count === 0) {
+      await userScrapCollection.drop();
     }
-    await userScrapCollection.updateOne({ user: username }, { $set: { keyWords: result.keyWords } });
-    return { message: '데이터 삭제 완료' };
+
+    return { message: "데이터 삭제 완료" };
   } catch (error) {
     console.error(error);
+    return { message: "데이터 삭제 에러" };
   }
 };
 
